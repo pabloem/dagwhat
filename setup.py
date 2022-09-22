1048e3601e324025e984fd518dfe36db97125597
Pablo E <pabloem@apache.org>
2022-09-21 22:44:41 -0400

    fixup setup

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
+"""Setup file for the Dagwhat library."""
+
 import os
 from setuptools import setup, find_packages
 
@@ -6,9 +27,7 @@ BUILD_ID = os.environ.get("BUILD_BUILDID", "0")
 setup(
     name="dagwhat",
     version="0.1" + "." + BUILD_ID,
-    # Author details
     packages=find_packages("dagwhat", exclude=['test']),
-    #package_dir={"": "src"},
     setup_requires=["apache-airflow"],
     tests_require=["pytest", "pytest-nunit", "pytest-cov"],
 )
